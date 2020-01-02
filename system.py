777eb87564df2155d43c129ae32680432c1ef275
Serkan Şahin <serkan.sahin@epigra.com>
2020-01-02 22:43:33 +0300

    specify winsound module

--- system.py
+++ system.py
@@ -1,13 +1,16 @@
 import platform
-import os
-import winsound
+
+
 
 def sound(sound):
     if (platform.system() == "Linux"):
+        import os
         merged = "aplay "+sound+"&"
         return os.system(merged)
     elif (platform.system() == "Darwin"):
+        import os
         merged = "afplay "+sound+"&"
         return os.system(merged)
-    elif (platform.system() ==  "Windows"):    
+    elif (platform.system() ==  "Windows"):
+        import winsound
         return winsound.PlaySound(sound,winsound.SND_ASYNC)
